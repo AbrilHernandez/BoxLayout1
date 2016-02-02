dac6c8ecdd87667dd105aa3b97e282771da7736f
LuzGonzalez <luzalgonzalezro@ittepic.edu.mx>
2016-02-02 13:18:09 -0700

    Definiendo la clase

---
+++ BoxLayout1.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.Container;
+import java.awt.Component;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+
+public class BoxLayout1 extends JFrame{
+
+public static void crearVentana(){
+JFrame frame=new JFrame("Ejemplo de Ventana con BoxLayout");
+frame.setVisible(true);
+}
+
+public static void main(String arg[]){
+crearVentana();
+}//main
+
+}//clase
