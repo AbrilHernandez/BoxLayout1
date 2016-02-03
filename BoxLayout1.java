acfeb0b1370eec6501b2ae7d8edabc15f4bedffc
AbrilHernandez <abyahernandezsa@ittepic.edu.mx>
2016-02-02 18:20:17 -0700

    Agregando Etiquetas

--- BoxLayout1.java
+++ BoxLayout1.java
@@ -1,33 +1,41 @@
 ﻿import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import java.awt.Container;
 import java.awt.Component;
 import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
-public class BoxLayoutTAP extends JFrame{
+public class BoxLayout1 extends JFrame{
 
-public static void agregarComponentes(Container pane){//parte 3-4
-pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));//parte 3
+public static void agregarComponentes(Container pane){
+pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 pane.setVisible(true);
-agregarBotones("Boton numero 1",pane);//parte 3
-agregarBotones("Boton numero 2",pane);//parte 3
-agregarBotones("Boton numero 3",pane);//parte 3
-}//parte 3-4
+agregarBotones("Boton numero 1",pane);
+agregarBotones("Boton numero 2",pane);
+agregarBotones("Boton numero 3",pane);
+agregarEtiqueta("Etiqueta 1", pane);
+}
 
-private static void agregarBotones(String texto, Container container){//parte 3
+
+public static void agregarEtiqueta(String texto, Container container){
+JLabel label=new JLabel(texto);
+label.setAlignmentY(Component.RIGHT_ALIGNMENT);
+container.add(label);
+
+}
+
+private static void agregarBotones(String texto, Container container){
  JButton boton=new JButton(texto);
 boton.setAlignmentX(Component.LEFT_ALIGNMENT);
 container.add(boton);
-}//parte 3
+}
 
-public static void crearVentana(){//parte 1
+public static void crearVentana(){
 JFrame frame=new JFrame("Ejemplo de Ventana con BoxLayout");
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//parte 2
-frame.pack();//parte 2
-frame.setSize(500, 400);//parte 2
-frame.setVisible(true);//parte 1
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.pack();
+frame.setSize(500, 400);
+frame.setVisible(true);
 agregarComponentes(frame.getContentPane());
 }
 public static void main(String arg[]){
@@ -36,4 +44,4 @@ crearVentana();
 
 }
 
-}
+}//clase
